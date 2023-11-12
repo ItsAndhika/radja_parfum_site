1d6ad29f061be038bc483248c3c36b3a82a39290
ItsAndhika <bambangindaryanto12@gmail.com>
2023-11-12 09:28:48 +0700

    Add footer

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+	return (
+		<footer className="pt-20 absolute">
+			<div className="w-screen h-20 bg-zinc-600/20 flex justify-center items-center">
+				<p className="text-sm font-semibold text-white text-center sm:text-base md:text-lg lg:text-lg 2xl:text-xl">
+					© Copyright 2023 Radja Parfum. All Rights Reserved
+				</p>
+			</div>
+		</footer>
+	);
+};
+
+export default Footer;
