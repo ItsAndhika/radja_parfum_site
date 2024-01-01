2feee9cee1632ad1e505511ba9cfe0ad25d04335
ItsAndhika <bambangindaryanto12@gmail.com>
2024-01-01 20:03:55 +0700

    Change the year of copyright

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
 	return (
 		<footer className="absolute bottom-0 flex justify-center items-center w-full h-10 bg-transparent z-10">
 			<p className="text-sm font-semibold text-black text-center sm:text-base md:text-lg lg:text-lg 2xl:text-xl">
-				© Copyright 2023 Mosto Elegant. All Rights Reserved
+				© Copyright 2024 Mosto Elegant. All Rights Reserved.
 			</p>
 		</footer>
 	);
